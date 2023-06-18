f9551ce6523e1180827fb52dfc803bce723c041d
JimyCalvo <jbcm1152@gmail.com>
2023-06-18 00:45:40 -0500

    compartir

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'photo-gps',
+  appId: 'com.example.app',
+  appName: 'App-Camera-GPS',
   webDir: 'www',
   server: {
     androidScheme: 'https'
